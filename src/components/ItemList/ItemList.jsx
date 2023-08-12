20ef3bfff3bbf16d587a126faacbd9ac964090ba
rodrigo gonzalez <roariel06@gmail.com>
2023-08-12 02:11:52 -0300

    Add link compotent to Item

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,16 +1,19 @@
 import './ItemList.css'
 import { Item } from '../Item/Item'
+import { Link } from 'react-router-dom'
 export function ItemList({ items = [] }) {
 
     return (
         <section className='il-container'>
             {items.map((item) => {
                 return (
-                    <Item
-                        key={item.id}
-                        image={item.urlImage}
-                        name={item.name}
-                    />
+                    <Link>
+                        <Item
+                            key={item.id}
+                            image={item.urlImage}
+                            name={item.name}
+                        />
+                    </Link>
                 )
             })}
         </section>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,9 +9,9 @@ export function NavBar() {
                     <Link to='/'><img src="src/assets/img/jump-force_logo.png" alt="logo-Jump" /></Link>
 
                     <nav className='nav-rout'>
-                        <a href="#"><h4>Home</h4></a>
-                        <a href="#"><h4>Mangas</h4></a>
-                        <a href="#"><h4>Manhwas</h4></a>
+                        <a className='category' href="#"><h4>Home</h4></a>
+                        <a className='category' href="#"><h4>Mangas</h4></a>
+                        <a className='category' href="#"><h4>Manhwas</h4></a>
                     </nav>
                 <div>
                     <div>
