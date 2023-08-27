e73e501c5dbf6024eb546ebf960f46c24a6739fc
rodrigo gonzalez <roariel06@gmail.com>
2023-08-27 13:00:34 -0300

    Add use of .env.loca a save the keys of firebase

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -1,14 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
+const apiKey = import.meta.env.VITE_APIKEY;
+const authDomain = import.meta.env.VITE_AUTHDOMAIN;
+const projectId = import.meta.env.VITE_PROJECTID;
+const storageBucket = import.meta.env.VITE_STORAGEBUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGINGSENDERID;
+const appId = import.meta.env.VITE_APPID;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2Qe0Lm13kujGaODaZCg5UJAF-nsFVUl8",
-  authDomain: "jump-mangas-eccomerce.firebaseapp.com",
-  projectId: "jump-mangas-eccomerce",
-  storageBucket: "jump-mangas-eccomerce.appspot.com",
-  messagingSenderId: "1016455218676",
-  appId: "1:1016455218676:web:846cf6cdcb37049eab65a1"
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 const app = initializeApp(firebaseConfig);
