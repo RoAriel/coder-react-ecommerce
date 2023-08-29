cd5452a033e084289d8be0352c87b2cf6eb2251d
rodrigo gonzalez <roariel06@gmail.com>
2023-08-28 22:52:54 -0300

    Update styles in Buy components

--- src/components/Buy/BuyOut.jsx
+++ src/components/Buy/BuyOut.jsx
@@ -48,12 +48,12 @@ export function BuyOut() {
 
     return (
         <section>
-            <h2>Finalize your purchase</h2>
+            <h2 className='text-format'>Finalize your purchase</h2>
             <form name="formulario" className="form-buy-data" onSubmit={handleSubmit(buy)}>
                 <input type="text" placeholder="Enter your Name"{...register("name")} />
                 <input type="text" placeholder="Enter your phone number"{...register("phone")} />
                 <input type="email" placeholder="Enter your Email"{...register("email")} />
-                <button type="submit" className='bts bt-add bg-gradiant'>Complete you buy!</button>
+                <button type="submit" className='bt-submit'>Complete you buy!</button>
             </form>
         </section>
     )
