29c34afa57cbd9d5306cec21cfa26fce92cb2a79
rodrigo gonzalez <roariel06@gmail.com>
2023-08-31 16:53:22 -0300

    Completed the BuyDetail
    Add new dependencies for table component of materialUI

--- src/components/BuyDetail/BuyDetail.jsx
+++ src/components/BuyDetail/BuyDetail.jsx
@@ -1,11 +1,49 @@
 import './BuyDetail.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
+
 export function BuyDetail({ order }) {
-    console.log(order)
     return (
-        <div className='color-text'>
-            <p>{order.buyer.contact.name}</p>
-            <p>{order.buyer.contact.phone}</p>
-            <p>{order.buyer.contact.email}</p>
-        </div>
+        <section className='dt-container'>
+            <h4 className='center'>Buy Detail</h4>
+        <TableContainer component={Paper}>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell className='dt-cell' align="left">Item</TableCell>
+                        <TableCell className='dt-cell' align="right">Price</TableCell>
+                        <TableCell className='dt-cell' align="right">Quantity</TableCell>
+                        <TableCell className='dt-cell' align="right">Total</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {order.items.map((item) => (
+                        <TableRow key={item.name}>
+                            <TableCell className='dt-cell' >{item.name}</TableCell>
+                            <TableCell className='dt-cell' align="right">{item.price}</TableCell>
+                            <TableCell className='dt-cell' align="right">{item.count}</TableCell>
+                            <TableCell className='dt-cell' align="right">$ {item.price * item.count}</TableCell>
+                        </TableRow>
+                    ))}
+                    <TableRow>
+                    <TableCell className='dt-cell'/>
+                    <TableCell className='dt-cell'/>
+                        <TableCell className='dt-cell' align="right" >Total Price: </TableCell>
+                        <TableCell className='dt-cell' align="right">$ {order.total}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <dir className='dt-end'></dir>
+        <Link to="/">
+                <button className='bt-home'> Go home and buy more!</button>
+            </Link>
+        </section>
     )
 }
